816b39bdb05e46cd29f4b71d01ec46c58378cff2
Adonis <bigirimanaadonis@gmail.com>
2025-09-25 12:36:23 +0300

    Send.F

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Append the new ID to the DOM
+      const body = document.querySelector("body");
+      body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch((error) => {
+      // Append error message to the DOM
+      const body = document.querySelector("body");
+      body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
